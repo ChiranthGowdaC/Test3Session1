5348aab2af69b02a445e00d3b342dfabd840bb17
Chiranth Gowda <97089430+ChiranthGowdaC@users.noreply.github.com>
2022-03-02 05:01:04 +0000

    p2

---
+++ p2original.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+struct _fraction
+{
+int num,den;
+};
+typedef _fraction Fraction
+{
+  
+}
+Fraction input_fraction();
+{
+  
+}
+Fraction Largest_fraction(Fractionf1,Fractionf2,Fractionf3)
+{
+  
+}
+void output(Fractionf1,Fractionf2,Fractionf3,Fraction largest)
+{
+  
+}
